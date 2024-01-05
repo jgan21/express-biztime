2fa311cda474846c7401a1dfb6c34b7a56c628d0
Nathan Rydel <nathanrydel@gmail.com>
2024-01-04 19:18:21 -0700

    Update import locations, GET /invoices tests

--- __tests__/invoices.test.js
+++ __tests__/invoices.test.js
@@ -5,7 +5,7 @@
 const request = require("supertest");
 
 const app = require("../app");
-const { createData } = require("../_test-common");
+const { createData } = require("../_test-setup");
 const db = require("../db");
 
 // before each test, clean out data
@@ -13,4 +13,19 @@ beforeEach(createData);
 
 afterAll(async () => {
   await db.end();
+});
+
+describe("GET /invoices Tests", function () {
+
+  test("Respond with array of invoices", async function () {
+    const response = await request(app).get("/invoices");
+    expect(response.body).toEqual({
+      invoices: [
+        { id: 1, comp_code: "apple" },
+        { id: 2, comp_code: "apple" },
+        { id: 3, comp_code: "ibm" },
+      ]
+    });
+  });
+
 });
